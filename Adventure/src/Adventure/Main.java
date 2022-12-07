2f8ccb3ec8cbd72f3b68e8a0b70dadf6d6d387d5
hessej25 <hessej25@gmail.com>
2022-12-07 16:23:02 -0600

    Player inputs enabled
    Players input and consequences to player actions added.

--- Adventure/src/Adventure/Main.java
+++ Adventure/src/Adventure/Main.java
@@ -28,10 +28,61 @@ public class Main {
 		
 		GAME:
 		while(running) {
+			System.out.println("----------------------------------");
+			//enemy spawn
+			int enemyHealth = rand.nextInt(maxEnemyHealth);
+			String enemy = enemies[rand.nextInt(enemies.length)];
+			System.out.println("\t# " + enemy + " has come to fight! #\n");
 			
+			//fight sequence
+			while(enemyHealth > 0){
+				//User display and actions
+				System.out.println("\tYour HP: " + hp);
+				System.out.println("\t" + enemy + "'s HP: " + enemyHealth);
+				System.out.println("\n\tChoose Action");
+				System.out.println("\t1. Attack");
+				System.out.println("\t2. Drink Potion");
+				System.out.println("\t3. Run Away");
+				
+				String input = sc.nextLine();
+				//outcomes from the user's input
+				if(input.equals("1")) {//fight enemy
+					int damageDealt = rand.nextInt(attackDmg);
+					int damageTaken = rand.nextInt(enemyAttackDmg); 
+				
+					enemyHealth -= damageDealt;
+					hp -= damageTaken;
+					
+					System.out.println("\t> You attacked " + enemy + "for " + damageDealt + " damage.");
+					System.out.println("\t> You were damaged for " + damageTaken);
+				
+					if(hp < 1) {
+						System.out.println("\t> You were slain in battle");
+						break;
+					}
+					
+				}
+				else if(input.equals("2")) {//drink potion
+					if(numHpPotions > 0) {
+						hp += potionHeal;
+						numHpPotions --;
+						System.out.println("\t> You drink a potion and restore " + potionHeal + " HP.");
+						System.out.println("\t Your total health is now " + hp + " HP.");
+						System.out.println("\t You have " + numHpPotions + "remaining.\n");
+					}
+					else {
+						System.out.println("\t> You have no more remaining potions. Fight more to find some.");
+					}
+				}
+				else if(input.equals("3")) {//run away
+					System.out.println("\tYou ran away from " + enemy);
+				}
+				else {
+					System.out.println("\tInvalid Input ");
+				}
+			}
 		}	
 		
-	
 	}
 	
 }
