2febe0ef1f6369864fe41050c415586ca644b9f2
hessej25 <hessej25@gmail.com>
2022-12-08 16:00:38 -0600

    Creating Objects started
    There is an issue with the images being used where the background of the image file is supposed to be transparent however it is not currently finding solution to this issue.

--- HesseLands/src/Main/UI.java
+++ HesseLands/src/Main/UI.java
@@ -3,7 +3,9 @@ package Main;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -14,12 +16,14 @@ public class UI {
 	JFrame window;
 	public JTextArea messageText;
 	public JPanel bgPanel[] = new JPanel[10];
-	
+	public JLabel bgLabel[] = new JLabel[10];
 	
 	public UI(GameManager gm) {
 		
 		this.gm = gm;
 		createMainField();
+		createBackground();
+		createObject();
 		window.setVisible(true);
 	}
 	
@@ -40,6 +44,32 @@ public class UI {
 		messageText.setFont(new Font("Book Antiqua", Font.PLAIN, 26));
 		window.add(messageText);
 		
+	}
+	
+	public void createBackground(){
+		
+		bgPanel[1] = new JPanel();
+		bgPanel[1].setBounds(50,50,700,350);
+		bgPanel[1].setBackground(Color.blue);
+		bgPanel[1].setLayout(null);
+		window.add(bgPanel[1]);
+		
+		bgLabel[1] = new JLabel();
+		bgLabel[1].setBounds(0,0,700,350);
+		
+		ImageIcon bgIcon = new ImageIcon(getClass().getClassLoader().getResource("forestpic.png"));
+		bgLabel[1].setIcon(bgIcon);
+	}
+	
+	public void createObject() {
+		
+		JLabel objectLabel = new JLabel();
+		objectLabel.setBounds(400,150,200,200);
+		ImageIcon objectIcon = new ImageIcon(getClass().getClassLoader().getResource("hutpic.png"));
+		objectLabel.setIcon(objectIcon);
+		
+		bgPanel[1].add(objectLabel);
+		bgPanel[1].add(bgLabel[1]);
 	}
 	
 }
