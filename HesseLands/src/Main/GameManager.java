e8ad3ab702e52819c616782bf86018c340c60e06
hessej25 <hessej25@gmail.com>
2022-12-08 12:59:47 -0600

    New Java Project Started
    This project is a point and click project where I found the basis for from youtube channel RyiSnow.

---
+++ HesseLands/src/Main/GameManager.java
@@ -0,0 +1,15 @@
+package Main;
+//This is based on the videos made by RyiSnow on Youtube. 
+public class GameManager {
+
+	UI ui = new UI(this);
+	
+	public static void main(String[] args) {
+		
+		new GameManager();
+
+	}
+	public GameManager() {
+	
+	}
+}
