dbd7dd99fa784e16050f6fca7deece8b77e7adee
hessej25 <hessej25@gmail.com>
2022-12-08 15:02:29 -0600

    Text Area added
    Text area added to the UI of the game, game is still in the building blocks stages

--- HesseLands/src/Main/UI.java
+++ HesseLands/src/Main/UI.java
@@ -1,13 +1,20 @@
 package Main;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class UI {
 
 	GameManager gm;
+	
 	JFrame window;
+	public JTextArea messageText;
+	public JPanel bgPanel[] = new JPanel[10];
+	
 	
 	public UI(GameManager gm) {
 		
@@ -22,6 +29,17 @@ public class UI {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.getContentPane().setBackground(Color.black);
 		window.setLayout(null);
+		
+		messageText = new JTextArea("Hello Josh");
+		messageText.setBounds(50, 400,700,150);
+		messageText.setBackground(Color.black);
+		messageText.setForeground(Color.white);
+		messageText.setEditable(false);
+		messageText.setLineWrap(true);
+		messageText.setWrapStyleWord(true);
+		messageText.setFont(new Font("Book Antiqua", Font.PLAIN, 26));
+		window.add(messageText);
+		
 	}
 	
 }
